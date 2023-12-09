3da3dbbd5b89f04271806654131e87eec449f42e
luka4684 <lb4684@student.uni-lj.si>
2023-12-09 12:30:37 +0100

    secrets

--- services/src/main/java/si/fri/rso/simplsrecka/user/services/externalAPI/EmailValidatorService.java
+++ services/src/main/java/si/fri/rso/simplsrecka/user/services/externalAPI/EmailValidatorService.java
@@ -11,11 +11,14 @@ import javax.enterprise.context.ApplicationScoped;
 @ApplicationScoped
 public class EmailValidatorService {
 
+    private String apiKey = System.getenv("EMAIL_VERIFIER_API_KEY");
+
     public boolean isValidEmail(String email) {
         try {
             HttpRequest request = HttpRequest.newBuilder()
                     .uri(URI.create("https://email-checker.p.rapidapi.com/verify/v1?email=" + email))
-                    .header("X-RapidAPI-Key", "d15df1c29bmsh9ff9c640b3dcbcep18a777jsna4a870b2b85e")
+                    //.header("X-RapidAPI-Key", "d15df1c29bmsh9ff9c640b3dcbcep18a777jsna4a870b2b85e")
+                    .header("X-RapidAPI-Key", apiKey)
                     .header("X-RapidAPI-Host", "email-checker.p.rapidapi.com")
                     .method("GET", HttpRequest.BodyPublishers.noBody())
                     .build();
